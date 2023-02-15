afd594f2e42b1c109b7df08a0b2cc9a555d83f1f
BuildTools <unconfigured@null.spigotmc.org>
2023-02-14 17:47:38 -0800

    init

---
+++ src/MainCode/java/net/fabricmod/serverutils/ServerUtils.java
@@ -0,0 +1,15 @@
+package net.fabricmod.serverutils;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ServerUtils implements ModInitializer {
+	public static final String MOD_ID = "serverutils";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
